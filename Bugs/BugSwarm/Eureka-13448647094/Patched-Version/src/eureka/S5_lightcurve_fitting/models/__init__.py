15474826292f929aa97f167417a39adf8e577e28
lashwi <alamani2@illinois.edu>
2024-03-05 16:03:45 -0600

    add bugswarm bug folder

---
+++ Bugs/BugSwarm/Eureka-13448647094/Patched-Version/src/eureka/S5_lightcurve_fitting/models/__init__.py
@@ -0,0 +1,8 @@
+from .BatmanModels import BatmanTransitModel, BatmanEclipseModel
+from .CentroidModel import CentroidModel
+from .ExpRampModel import ExpRampModel
+from .GPModel import GPModel
+from .Model import Model, CompositeModel
+from .PolynomialModel import PolynomialModel
+from .SinusoidPhaseCurve import SinusoidPhaseCurveModel
+from .StepModel import StepModel
