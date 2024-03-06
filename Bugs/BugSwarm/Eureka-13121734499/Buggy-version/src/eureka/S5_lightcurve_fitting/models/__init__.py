5a135de60f46baa7329469ed8894e3389c999483
lashwi <alamani2@illinois.edu>
2024-03-05 19:52:17 -0600

    remove folder

--- Bugs/BugSwarm/Eureka-13121734499/Buggy-version/src/eureka/S5_lightcurve_fitting/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .BatmanModels import BatmanTransitModel, BatmanEclipseModel
-from .CentroidModel import CentroidModel
-from .ExpRampModel import ExpRampModel
-from .GPModel import GPModel
-from .Model import Model, CompositeModel
-from .PolynomialModel import PolynomialModel
-from .SinusoidPhaseCurve import SinusoidPhaseCurveModel
-from .StepModel import StepModel
--- Bugs/BugSwarm/Eureka-13349239705/Patched-Version/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-# Some bits of code to help VS Code run tests automatically
-
-import os
-# VS Code runs tests from the root directory, so need to chdir into
-# the tests directory
-if os.getcwd().split(os.sep)[-1] != "tests":
-    os.chdir('tests')
